901d00dd62b156696d255a6659ab0fac2ed4618b
kirupa <kirupakaran.qmax@gmail.com>
2018-05-24 18:55:22 +0530

    project

---
+++ app/src/main/java/kirupa/com/dynamictab/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package kirupa.com.dynamictab;
+
+/**
+ * Created by kirupa on 24/5/18.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private String title[] = {"TAB 1", "TAB 2", "TAB 3","TAB 4","TAB 5","TAB 6"};
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TabFragment.getInstance(position);
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+}
